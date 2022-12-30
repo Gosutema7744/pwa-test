e666339d75bd4991d308acfe4c2ea9c5c5bf8ac0
Gosutema7744 <121618388+Gosutema7744@users.noreply.github.com>
2022-12-30 10:08:40 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,6 +28,20 @@ if ('serviceWorker' in navigator) {
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
 
 window.addEventListener('DOMContentLoaded', async () => {
   // Set up the editor
